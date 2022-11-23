40723c3cf915f04232f12b32086884f412b015ff
jaikrishnaverma <jaikrishnaverma>
2022-11-23 11:56:51 +0530

    completed

--- src/app/features/counter/countreSlice.js
+++ src/app/features/counter/countreSlice.js
@@ -1,23 +1,93 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 const initialState = {
-    data: []
+    loading: false,
+    data: [],
+    error: ''
 }
+export const fetchProduct = createAsyncThunk('product/fetchProduct',
+    async type => {
+        const response = await axios.get('https://dummyjson.com/carts/1')
+        console.log("prd", response)
+        return response.data.products;
+    }
+)
+export const addProduct = createAsyncThunk('product/addProduct',
+    async type => {
+        const json = await axios.post('https://dummyjson.com/carts/add', {
+            userId: 1,
+            products: type
+        })
+        return json.data.products
+    })
+
+export const updateProduct = createAsyncThunk('product/updateProduct',
+    async type => {
+        const json = await axios.put('https://dummyjson.com/carts/1', {
+            products: [type]
+        })
+        return json.data.products
+    })
+
+    export const deleteCart = createAsyncThunk('product/deleteCart',
+    async type => {
+        const json = await axios.delete('https://dummyjson.com/carts/1')
+        return json
+    })
+
 
 const database = createSlice({
     name: "product",
     initialState,
     reducers: {
-        AddData: (state, action) => {
-            state.data.push({ ...action.payload })
+
+
+    },
+    extraReducers: {
+        [fetchProduct.pending]: (state, action) => {
+            state.loading = true
         },
-        UpdateData: (state, action) => {
-            state.data[action.payload.index] = { ...action.payload.data }
+        [fetchProduct.fulfilled]: (state, action) => {
+            state.loading = false
+            state.data = action.payload
+            state.error = ''
         },
-        DeleteData: (state, action) => {
-            state.data.splice(action.payload.index, 1)
+        [fetchProduct.rejected]: (state, action) => {
+            state.loading = false
+            state.data = []
+            state.error = action.error.message
+        },
+        [addProduct.pending]: (state, action) => {
+            state.error = ''
+        },
+        [addProduct.fulfilled]: (state, action) => {
+            state.error = 'Items added successfully.'
+            state.data = action.payload
+        },
+        [addProduct.rejected]: (state, action) => {
+            state.error = 'Error in adding to cart.'
+        },
+        [updateProduct.pending]: (state, action) => {
+            state.error = ''
+        },
+        [updateProduct.fulfilled]: (state, action) => {
+            state.error = 'Items Updated successfully.'
+            state.data = action.payload
+        },
+        [updateProduct.rejected]: (state, action) => {
+            state.error = 'Error in Update to cart.'
+        },
+        [deleteCart.fulfilled]: (state, action) => {
+            state.error = 'Deleted Succesfully.'
+            state.data=[]
+        },
+        [deleteCart.pending]: (state, action) => {
+            state.error = ''
+        },
+        [deleteCart.rejected]: (state, action) => {
+            state.error = 'Error in Delete to cart.'
         }
     }
 });
-export  default database.reducer
-export const {AddData,UpdateData,DeleteData}=database.actions
+export default database.reducer
+export const { AddData, UpdateData, DeleteData } = database.actions
--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Store } from '@reduxjs/toolkit'
-import database, { store } from '../app/store'
-import { AddData, UpdateData, DeleteData } from '../app/features/counter/countreSlice'
+import { fetchProduct, addProduct, updateProduct,deleteCart } from '../app/features/counter/countreSlice'
 const Main = () => {
-
-    const state = useSelector(state => state.database.data)
+    const action = useRef()
+    const idRef = useRef()
+    const quantityRef = useRef()
+    const state = useSelector(state => state.database)
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchProduct())
+    }, [])
 
     const submitter = (e) => {
         e.preventDefault()
-        dispatch(AddData({ Id: e.target.id.value, quantity: e.target.quantity.value }))
+        if (e.target.action.value == 'add') {
+            dispatch(addProduct([...state.data, { id: e.target.id.value, quantity: e.target.quantity.value }]))
+        } else {
+            dispatch(updateProduct([{ id: e.target.id.value, quantity: e.target.quantity.value }]))
+        }
+        action.current.value = "add"
+        idRef.current.value = ''
+        quantityRef.current.value = ''
     }
-    console.log(state)
+
+    const updateProducts = (id, quant) => {
+        action.current.value = "update"
+        idRef.current.value = id
+        quantityRef.current.value = quant
+    }
+
     return (
         <>
             <div className="full col flexAIC p2">
@@ -20,40 +37,56 @@ const Main = () => {
                     <h1 className='p3 m1 linebottom hp0 hm0'>Add to cart</h1>
                     <form onSubmit={submitter} className="full col m2 hm0 linebottom">
                         <label htmlFor="" className='m1 hm0'>Product Id:</label>
-                        <input type="number" className='brd line p1 m1 hm0' name="id" placeholder='Product Id' />
+                        <input type="number" ref={idRef} className='brd line p1 m1 hm0' name="id" placeholder='Product Id' />
                         <label htmlFor="" className='m1 hm0'>Quantity:</label>
-                        <input type="number" className='brd line p1 m1 hm0' name="quantity" placeholder='Quantity' />
+                        <input type="hidden" ref={action} name='action' value="add" />
+                        <input type="number" ref={quantityRef} className='brd line p1 m1 hm0' name="quantity" placeholder='Quantity' />
                         <button type='submit' className='btn sbg m2 hm0'>Add to cart</button>
                     </form>
 
                 </div>
-                <div className="w40 col">
-                    <h1 className='p3 m1 linebottom hp0 hm0'>Shopping cart</h1>
-                    <div className="full col m2 hm0 ">
-                        <button className='btn pbg  hm0'>Delete Cart</button>
+                <div className="w40 col flexAIC">
+                    <div className="full row">
+                        <h1 className='p3 m1 linebottom hp0 hm0'>Shopping cart</h1>
                     </div>
+                    <div className="full col m2 hm0 ">
+                        <button className='btn pbg  hm0' onClick={()=>dispatch(deleteCart())}>Delete Cart</button>
+                    </div>
+                    {
+                        (state.error !== '') ?
+                            <div className={`notify w100 success ${(state.error.split(' ')[0] == "Error") ? "error" : "success"}`}>
+                                <span className='fw'>{(state.error.split(' ')[0] == "Error") ? "Error" : "Success"}:</span> {state.error}</div> :
+                            <div></div>
 
-                    <table className="m2 hm0">
-                        <thead>
-                            <tr>
-                                <th>S.No.</th>
-                                <th>Title</th>
-                                <th>Quality</th>
-                                <th>Action</th>
-                                <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><p>1</p></td>
-                                <td><p>Malai Maxi Dress</p></td>
-                                <td className='line brd'><p>1</p></td>
-                                <td className='lineS brd bn'><button >update1</button></td>
-                                <td><p>50</p></td>
-                            </tr>
-                        </tbody>
+                    }
+                    {
+                        (state.loading) ? <img src="Iphone-spinner-2.gif" width="100px" alt="" /> : <table className="m2 hm0 full">
+                            <thead>
+                                <tr>
+                                    <th>S.No.</th>
+                                    <th>Id</th>
+                                    <th>Title</th>
+                                    <th>Quality</th>
+                                    <th>Action</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    state.data.map((y, i) => <tr>
+                                        <td><p>{i}</p></td>
+                                        <td><p>{y.id}</p></td>
+                                        <td><p>{y.title}</p></td>
+                                        <td className='line brd'><p>{y.quantity}</p></td>
+                                        <td className='lineS brd bn' onClick={() => updateProducts(y.id, y.quantity)}><button >update</button></td>
+                                        <td><p>{y.price}</p></td>
+                                    </tr>)
 
-                    </table>
+                                }
+                            </tbody>
+
+                        </table>
+                    }
                 </div>
             </div>
         </>
